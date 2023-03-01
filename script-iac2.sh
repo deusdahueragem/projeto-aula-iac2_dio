8864449aff0aacd266e956fd89ef783eec200e23
Luiz Eduardo da Silva <93293627+deusdahueragem@users.noreply.github.com>
2023-03-01 19:37:32 -0300

    init

---
+++ script-iac2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Atualizando Servidor
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+cd /tmp
+#Baixando arquivos do site
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
